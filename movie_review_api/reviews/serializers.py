d7bc13169fd22fd1f7e7d17cb409798c30a5d3d5
Sephanie96 <koutanyi@yahoo.com>
2024-10-05 14:37:06 +0100

    Implement permission checks

--- movie_review_api/reviews/serializers.py
+++ movie_review_api/reviews/serializers.py
@@ -6,7 +6,28 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+
+        password = validated_data.get('password', None)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 
 class MovieSerializer(serializers.ModelSerializer):
--- movie_review_api/reviews/views.py
+++ movie_review_api/reviews/views.py
@@ -1,28 +1,38 @@
 from django.shortcuts import render
-from rest_framework import generics, serializers
+from rest_framework import generics
 from .models import Movie, Review
 from .serializers import MovieSerializer, ReviewSerializer, UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly
 from rest_framework import status
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 
 
-class PublicViewSet(viewsets.ModelViewSet):
-    permission_classes = [AllowAny]  # No authentication needed for this view
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return []
+        return super().get_permissions()
+
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
 
     def perform_create(self, serializer):
         # Automatically associate the review with the logged-in user
         movie_id = self.request.data.get('movie')  # Get the movie ID from request
         try:
             movie = Movie.objects.get(id=movie_id)
+            # Set the user who created the review as the logged-in user
             serializer.save(user=self.request.user, movie=movie)
         except Movie.DoesNotExist:
             return Response({"error": "Movie does not exist."}, status=status.HTTP_400_BAD_REQUEST)
@@ -35,6 +45,11 @@ class ReviewViewSet(viewsets.ModelViewSet):
     @swagger_auto_schema(operation_summary="Create a review", operation_description="Create a new review for a movie.")
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
+
+
+
+class PublicViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]  # No authentication needed for this view
 
 
 class MovieViewSet(viewsets.ModelViewSet):
@@ -67,9 +82,5 @@ class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
 
 
